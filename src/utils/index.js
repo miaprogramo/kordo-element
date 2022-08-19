c446a17b4a50ccb6663a0269f4d56134316de35f
miaprogramo <mialetero@163.com>
2022-08-19 22:29:44 +0800

    add cloneDeep

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
-import clonedeep from "./clonedeep";
+import cloneDeep from "./cloneDeep";
 import debounce from "./debounce";
 import throttle from "./throttle";
 
 export const isClient = typeof window !== "undefined";
 
-export { clonedeep, debounce, throttle };
+export { cloneDeep, debounce, throttle };
